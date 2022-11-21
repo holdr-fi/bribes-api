f4478855cb3c644ea4edbcf2c7c11d8238211634
kyzooghost <jeffersonzjtang@gmail.com>
2022-11-21 17:07:10 +1100

    added aurora config

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -8,6 +8,9 @@ if (typeof process.env.MAINNET_URL === 'undefined') {
 if (typeof process.env.MUMBAI_URL === 'undefined') {
   throw new Error('Invalid MUMBAI_URL in .env');
 }
+if (typeof process.env.AURORA_URL === 'undefined') {
+  throw new Error('Invalid AURORA_URL in .env');
+}
 if (typeof process.env.CHAIN_ID === 'undefined') {
   throw new Error('Invalid CHAIN_ID in .env');
 }
@@ -20,6 +23,7 @@ if (typeof process.env.TABLE_NAME === 'undefined') {
 
 export const MAINNET_URL = process.env.MAINNET_URL;
 export const MUMBAI_URL = process.env.MUMBAI_URL;
+export const AURORA_URL = process.env.AURORA_URL;
 export const CHAIN_ID = process.env.CHAIN_ID;
 export const BUCKET_NAME = process.env.BUCKET_NAME;
 export const TABLE_NAME = process.env.TABLE_NAME;
--- src/network/provider.ts
+++ src/network/provider.ts
@@ -1,5 +1,5 @@
 import { providers } from 'ethers';
-import { CHAIN_ID, MAINNET_URL, MUMBAI_URL } from '../constants';
+import { CHAIN_ID, MAINNET_URL, MUMBAI_URL, AURORA_URL } from '../constants';
 
 let RPC_URL: string;
 
@@ -9,6 +9,9 @@ switch (CHAIN_ID) {
     break;
   case '80001':
     RPC_URL = MUMBAI_URL;
+    break;
+  case '1313161554':
+    RPC_URL = AURORA_URL;
     break;
   default:
     throw new Error('Invalid CHAIN_ID');
